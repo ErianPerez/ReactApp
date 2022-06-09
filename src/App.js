ade46e03db9024a2371a8527b659d9aee7f962b5
erianperez <erianperez3@gmail.com>
2022-06-09 10:32:46 -0300

    Primera Version

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ErianPerez"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pasate por mi Github!
         </a>
       </header>
     </div>
---
+++ src/Caja.js
@@ -0,0 +1,10 @@
+function Caja(props) {
+    return(
+        <div className = "Caja">
+            <p>
+                Bienvenido {props.nombre}! Esta es mi pagina 
+            </p>      
+        </div>
+        );
+}
+export default Caja;
